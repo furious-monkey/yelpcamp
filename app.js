8560a19f68b232be248121bdc6e0e9c7fa05536d
furious-monkey <daniel.j.hahn1105@gmail.com>
2019-12-22 19:48:06 +0100

    gjysma1

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+// <%- include("partials/header") %>
+
+app.set('view engine', 'ejs');
+
+app.get("/", function (req, res) {
+    res.render("landing");
+});
+
+app.get("/campgrounds", function (req, res) {
+    var campgrounds = [
+        { name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_960_720.jpg" },
+        { name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2018/12/24/22/19/camping-3893587_960_720.jpg" },
+        { name: "Montain Goat's Rest", image: "https://cdn.pixabay.com/photo/2017/08/04/20/04/camping-2581242_960_720.jpg" }
+    ]
+    res.render("campgrounds", { camps: campgrounds })
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP || '127.0.0.1', function () {
+    console.log("Filluam djema!");
+});
