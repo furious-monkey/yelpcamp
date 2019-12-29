8656c8d02fe8d2a2833fa13625a8dbc50026f445
furious-monkey <daniel.j.hahn1105@gmail.com>
2019-12-29 21:09:25 +0100

    local external stylesheet at public/stylesheet

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true,
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function (req, res) {
     res.render("landing");
