c80cf1dfe913ce88487de30312c56cce0625a16a
furious-monkey <daniel.j.hahn1105@gmail.com>
2020-01-02 17:41:52 +0100

    campground Authorization

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -35,17 +35,13 @@ router.get("/:id", function(req, res) {
     });
 });
 
-router.get("/:id/edit", function(req, res) {
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
   Campground.findById(req.params.id, (err, foundCampground) => {
-    if (err) {
-      res.redirect("/campgrounds");
-    } else {
-      res.render("campgrounds/edit", { campground: foundCampground });
-    }
+    res.render("campgrounds/edit", { campground: foundCampground });
   });
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", checkCampgroundOwnership, (req, res) => {
   Campground.findOneAndUpdate(req.params.id, req.body.campground, err => {
     if (err) {
       res.redirect("/campgrounds");
@@ -55,7 +51,7 @@ router.put("/:id", (req, res) => {
   });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", checkCampgroundOwnership, (req, res) => {
   Campground.findOneAndDelete(req.params.id, err => {
     if (err) {
       res.redirect("/campgrounds");
@@ -70,6 +66,24 @@ function isLoggedIn(req, res, next) {
     return next();
   }
   res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Campground.findById(req.params.id, (err, foundCampground) => {
+      if (err) {
+        res.redirect("back");
+      } else {
+        if (foundCampground.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
 }
 
 module.exports = router;
