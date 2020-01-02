b0e55e2546c7626710c124f07a43493f0c6e82e7
furious-monkey <daniel.j.hahn1105@gmail.com>
2020-01-02 17:18:26 +0100

    v10

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require("express"),
   mongoose = require("mongoose"),
   passport = require("passport"),
   LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
   // Campground = require("./models/campground"),
   // Comment = require("./models/comment"),
   User = require("./models/user");
@@ -25,6 +26,7 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 
 //Passport CONFIG
 app.use(
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -35,6 +35,40 @@ router.get("/:id", function(req, res) {
     });
 });
 
+router.get("/:id/edit", function(req, res) {
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.render("campgrounds/edit", { campground: foundCampground });
+    }
+  });
+});
+
+router.put("/:id", (req, res) => {
+  Campground.findOneAndUpdate(
+    req.params.id,
+    req.body.campground,
+    (err, camp) => {
+      if (err) {
+        res.redirect("/campgrounds");
+      } else {
+        res.redirect(`/campgrounds/${req.params.id}`);
+      }
+    }
+  );
+});
+
+router.delete("/:id", (req, res) => {
+  Campground.findOneAndDelete(req.params.id, err => {
+    if (err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
